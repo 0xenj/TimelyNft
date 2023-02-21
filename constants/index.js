b4fe563a6bf8b70dacffff87b50c687147839d37
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-02-21 16:52:36 +0100

    push

--- constants/index.js
+++ constants/index.js
@@ -11,4 +11,9 @@ module.exports = {
   CONTRACT_NAME: "Timely",
   TOKEN_NAME: "Timely",
   TOKEN_SYMBOL: "TYME",
+  CONTRACT_CONTRACT_URI: "ipfs://test123456test.test/test123contractURI.json",
+  CONTRACT_BASE_URI: "ipfs://test123456test.test/",
+  CONTRACT_EXTENSION_URI: ".json",
+  MAX_SUPPLY: 15000,
+  MINT_PRICE: 5,
 };
